fc2691be8651eb7d840ce461b9aefeaa32bf44c2
Ifeoluwa Sulaiman <ifeoluwasulaiman30@gmail.com>
2026-02-27 21:54:20 +0100

    fix: fixed the setup tools and setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         "Changelog": "https://github.com/DOOMSDAY101/bugtrace/blob/main/CHANGELOG.md",
     },
     packages=find_packages(exclude=["tests", "tests.*"]),
-    include_package_data=True,  
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
